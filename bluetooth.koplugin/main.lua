b7b71ed0e61c234c0b3c862e23ffafa349f04407
sabrina553 <general@sabrinalily.uk>
2026-07-04 17:03:53 +0100

    refactor(main): clean up logging messages and remove unused imports

--- bluetooth.koplugin/main.lua
+++ bluetooth.koplugin/main.lua
@@ -1,12 +1,10 @@
 local logger = require("logger")
---local Device = require("device")
-local Dispatcher = require("dispatcher") -- luacheck:ignore
 local _ = require("gettext")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 
 local BluetoothMenu = require("bluetooth/ui/menu")
 local controller = require("bluetooth/controller/controller")
-local devices = require("bluetooth/controller/devices")
+
 
 ---@class Bluetooth
 ---@field ui any This is a ReaderUI
@@ -33,26 +31,26 @@ function Bluetooth:init()
 
     self.ui.menu:registerToMainMenu(self.menu)
 
-    logger:dbg("Bluetooth.koplugin Initialized")
+    logger:dbg("Bluetooth.koplugin: Initialized")
 end
 
 function Bluetooth:onExit()
-    logger.info("Device exiting, disabling bluetooth")
+    logger.dbg("Bluetooth.koplugin: Device exiting, disabling bluetooth")
     self.controller:disable()
 end
 
 function Bluetooth:onSuspend()
-    logger.info("Device suspending, disabling bluetooth")
+    logger.dbg("Bluetooth.koplugin: Device suspending, disabling bluetooth")
     self.controller:disable()
 end
 
 function Bluetooth:onPause()
-    logger.info("Device pausing, disabling bluetooth")
+    logger.dbg("Bluetooth.koplugin: Device pausing, disabling bluetooth")
     self.controller:disable()
 end
 
 function Bluetooth:onResume()
-    logger.info("Device resumed, Restoring Bluetooth")
+    logger.dbg("Bluetooth.koplugin: Device resumed, Restoring Bluetooth")
     self.controller:enableWhenDisabled()
 end
 
