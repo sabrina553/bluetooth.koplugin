1e4e6d0e2b8a27d231de0ba4919cdb025ec61250
sabrina553 <general@sabrinalily.uk>
2026-07-07 09:51:46 +0100

    fix(meta): revert version to 0.0.3

--- bluetooth.koplugin/_meta.lua
+++ bluetooth.koplugin/_meta.lua
@@ -2,6 +2,6 @@ local _ = require("gettext")
 return {
     fullname = _("Bluetooth"),
     description = _([[General purpose bluetooth plugin for koreader.]]),
-    version = "0.0.3.dev.3",
+    version = "0.0.3",
     repository = "sabrina553/bluetooth.koplugin"
 }
